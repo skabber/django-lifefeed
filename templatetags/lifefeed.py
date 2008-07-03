f000126f9fba3eaf2e4e0d0c045b6f86b8e381f2
skabber <skabber@14b06206-1851-0410-b3c6-6f2e7453594b>
2008-07-03 05:43:37 +0000

    initial import
    git-svn-id: https://django-lifefeed.googlecode.com/svn/trunk@2 14b06206-1851-0410-b3c6-6f2e7453594b

---
+++ templatetags/lifefeed.py
@@ -0,0 +1,26 @@
+import re
+from django import template
+from django.utils.encoding import smart_str, force_unicode
+from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
+from friendfeed import FriendFeed
+
+register = template.Library()
+
+re_twitter = re.compile(r"(@(.+?\b))")
+sub_twitter =  r'<a href="http://twitter.com/\2">\1</a>'
+re_link = re.compile(r"(http.*)\s*")
+sub_link = r'<a href="\1">\1</a>'
+
+def linkify(value):
+    value = re.sub(re_link, sub_link, value)
+    value = re.sub(re_twitter, sub_twitter, value)
+    return mark_safe(force_unicode(smart_str(value)))
+
+def lifefeed(username):
+    stream = FriendFeed().fetch_user_feed(username)
+    items = stream["entries"]
+    return render_to_string("friendfeed.html", {"items": items})
+
+register.filter(linkify)
+register.simple_tag(lifefeed)
